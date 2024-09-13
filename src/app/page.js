13d518074a756863b22167723dacd892ad96c99e
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-09-13 10:42:23 +0200

    card 2 styles added and basic state for selected message

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,26 @@
+'use client'
+
+import { useState } from 'react'
+
 export default function Home() {
+	const [selectedRating, setSelectedRating] = useState(0)
+
+	function setRatingOne() {
+		setSelectedRating(1)
+	}
+	function setRatingTwo() {
+		setSelectedRating(2)
+	}
+	function setRatingThree() {
+		setSelectedRating(3)
+	}
+	function setRatingFour() {
+		setSelectedRating(4)
+	}
+	function setRatingFive() {
+		setSelectedRating(5)
+	}
+
 	return (
 		<main>
 			<section className='card-container'>
@@ -16,11 +38,11 @@ export default function Home() {
 						offering!`}
 					</p>
 					<div className='rating-button-container'>
-						<button>1</button>
-						<button>2</button>
-						<button>3</button>
-						<button>4</button>
-						<button>5</button>
+						<button onClick={setRatingOne}>1</button>
+						<button onClick={setRatingTwo}>2</button>
+						<button onClick={setRatingThree}>3</button>
+						<button onClick={setRatingFour}>4</button>
+						<button onClick={setRatingFive}>5</button>
 					</div>
 					<button className='submit-btn'>SUBMIT</button>
 				</div>
@@ -28,7 +50,9 @@ export default function Home() {
 			<section className='card-container'>
 				<div className='card-2'>
 					<img src='illustration-thank-you.svg' alt='' role='none' />
-					<p>you selected *** out of 5</p>
+					<p className='card--rating-selected'>
+						you selected {selectedRating} out of 5
+					</p>
 					<h2>Thank you!</h2>
 					<p>
 						{`We appreciate you taking the time to give a rating. If you
