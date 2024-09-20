0f41ad6a05e35a72ae56b421dbf1bf949acc60e7
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-09-20 11:23:19 +0200

    rating number validation and error message fixed

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,21 +1,15 @@
 'use client'
 
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
-	// let navigate = useNavigate()
-	// let rating = { selectedRating }
-
-	function handleClick() {
-		history.push('/thank-you', { rating })
-	}
-
 	const [selectedRating, setSelectedRating] = useState(0)
+	const [errorBoolean, setErrorBoolean] = useState(false)
+	const [errorMessage, setErrorMessage] = useState({})
 
+	// sets the value rating through state change
 	function setRatingOne() {
 		setSelectedRating(1)
-		console.log(selectedRating)
 	}
 	function setRatingTwo() {
 		setSelectedRating(2)
@@ -28,6 +22,48 @@ export default function Home() {
 	}
 	function setRatingFive() {
 		setSelectedRating(5)
+	}
+
+	useEffect(() => {
+		// checks to see if error message is present and then sets the error boolean
+		console.log(selectedRating)
+		let errorMessage = document.getElementById('errorMessage')
+		if (errorMessage === null) {
+			console.log('error message = false')
+			setErrorBoolean(false)
+			return
+		} else {
+			console.log('error message = true')
+			setErrorBoolean(true)
+			errorMessage.focus()
+		}
+	}, [errorBoolean])
+
+	// logs what number is selected
+	useEffect(() => {
+		console.log(selectedRating)
+		// validateRating()
+	}, [selectedRating])
+
+	// checks the rating number on submit and sets the corresponding error message and sets the error boolean
+	const validateRating = () => {
+		let errorMessage = {}
+		if (selectedRating === 0) {
+			setErrorBoolean(true)
+			errorMessage.text = 'Please select a rating'
+			setErrorMessage(errorMessage)
+		} else if (selectedRating >= 1) {
+			console.log(`selected rating is`, selectedRating)
+			errorMessage.text = ''
+			setErrorMessage(errorMessage)
+			setErrorBoolean(false)
+		}
+	}
+
+	//
+	const submitValidation = (e) => {
+		e.preventDefault()
+		validateRating()
 	}
 
 	return (
@@ -47,74 +83,84 @@ export default function Home() {
 						offering!`}
 					</p>
 
-					<form className='rating-form'>
-						<label for='rating-1'>
-							<input
-								type='radio'
-								name='rating'
-								id='rating-1'
-								value='1'
-							/>
-							<div
-								className='rating-number'
-								onClick={setRatingOne}>
-								1
+					<form className='rating-form' onSubmit={submitValidation}>
+						{errorMessage.text && (
+							<div className='error-message' id='errorMessage'>
+								{errorMessage.text}
 							</div>
-						</label>
-						<label for='rating-2'>
-							<input
-								type='radio'
-								name='rating'
-								id='rating-2'
-								value='2'
-							/>
-							<div
-								className='rating-number'
-								onClick={setRatingTwo}>
-								2
-							</div>
-						</label>
-						<label for='rating-3'>
-							<input
-								type='radio'
-								name='rating'
-								id='rating-3'
-								value='3'
-							/>
-							<div
-								className='rating-number'
-								onClick={setRatingThree}>
-								3
-							</div>
-						</label>
-						<label for='rating-4'>
-							<input
-								type='radio'
-								name='rating'
-								id='rating-4'
-								value='4'
-							/>
-							<div
-								className='rating-number'
-								onClick={setRatingFour}>
-								4
-							</div>
-						</label>
-						<label for='rating-5'>
-							<input
-								type='radio'
-								name='rating'
-								id='rating-5'
-								value='5'
-							/>
-							<div
-								className='rating-number'
-								onClick={setRatingFive}>
-								5
-							</div>
-						</label>
+						)}
+
+						<div className='rating-form__btn-container'>
+							<label htmlFor='rating-1'>
+								<input
+									type='radio'
+									name='rating'
+									id='rating-1'
+									value='1'
+								/>
+								<div
+									className='rating-number'
+									onClick={setRatingOne}>
+									1
+								</div>
+							</label>
+							<label htmlFor='rating-2'>
+								<input
+									type='radio'
+									name='rating'
+									id='rating-2'
+									value='2'
+								/>
+								<div
+									className='rating-number'
+									onClick={setRatingTwo}>
+									2
+								</div>
+							</label>
+							<label htmlFor='rating-3'>
+								<input
+									type='radio'
+									name='rating'
+									id='rating-3'
+									value='3'
+								/>
+								<div
+									className='rating-number'
+									onClick={setRatingThree}>
+									3
+								</div>
+							</label>
+							<label htmlFor='rating-4'>
+								<input
+									type='radio'
+									name='rating'
+									id='rating-4'
+									value='4'
+								/>
+								<div
+									className='rating-number'
+									onClick={setRatingFour}>
+									4
+								</div>
+							</label>
+							<label htmlFor='rating-5'>
+								<input
+									type='radio'
+									name='rating'
+									id='rating-5'
+									value='5'
+								/>
+								<div
+									className='rating-number'
+									onClick={setRatingFive}>
+									5
+								</div>
+							</label>
+						</div>
+						<div>
+							<button className='submit-btn'>SUBMIT</button>
+						</div>
 					</form>
-					<button className='submit-btn'>SUBMIT</button>
 				</div>
 			</section>
 		</main>
